5ebc3b3e185846eb3476d9468ed303040f8699b1
Broump <DanielMuenstermann18@gmail.com>
2022-02-26 20:03:11 +0100

    deploy

---
+++ client/build/precache-manifest.44eebdf71ec0f1f78bcbd9902e0b81eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f11b378adb7974c348cc",
+    "url": "/static/css/main.a7026eff.chunk.css"
+  },
+  {
+    "revision": "f11b378adb7974c348cc",
+    "url": "/static/js/main.f11b378a.chunk.js"
+  },
+  {
+    "revision": "cc9f86cf9e3e2cf30d0f",
+    "url": "/static/css/1.9e59ef71.chunk.css"
+  },
+  {
+    "revision": "cc9f86cf9e3e2cf30d0f",
+    "url": "/static/js/1.cc9f86cf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3de0184f3e602d30d923a43ba3061b1b",
+    "url": "/index.html"
+  }
+];
